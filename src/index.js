41337b2c620ed6326316e3d56d108cf81dc9b7ba
Nathan Fiscus <nathan_fiscus@hotmail.com>
2021-01-05 21:29:11 -0700

    fix pwa configuration

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import AppWrapper from "./AppWrapper";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+//import reportWebVitals from './reportWebVitals';
 import swConfig from "./swconfig";
 
 ReactDOM.render(
@@ -15,4 +16,6 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register(swConfig);
+serviceWorkerRegistration.register(swConfig);
+
+//reportWebVitals();
